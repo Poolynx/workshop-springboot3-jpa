db93eebee4245f356b6c6427ea2d583b1b070ebb
Poolynx <wesleyrab2020@gmail.com>
2024-01-31 15:50:24 -0300

    H2 database, test profile, JPA

--- src/main/java/com/wesley/course/entities/User.java
+++ src/main/java/com/wesley/course/entities/User.java
@@ -3,9 +3,20 @@ package com.wesley.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+//A palavra User é uma word reservada, então iremos alterar o nome da tabela.
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
