a2ed9860464cdf2fffe78a71bc32bb6a89acf271
Poolynx <wesleyrab2020@gmail.com>
2024-01-31 14:25:29 -0300

    Entidade User e seu resource

---
+++ src/main/java/com/wesley/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.wesley.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.wesley.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1L, "Wesley", "wesley@gmail.com", "99999", "123");
+		return ResponseEntity.ok().body(u);
+	}
+}
