70fc00d49d3f12a9fab4ac07951281df555d535a
Poolynx <wesleyrab2020@gmail.com>
2024-02-02 09:28:36 -0300

    OrderItem, many-to-many association with extra attributes

---
+++ src/main/java/com/wesley/course/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.wesley.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wesley.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+
+}
